b45932ce4b8a921c82b02d068735c9cff853d04e
Алексей <@DESKTOP-V0RD6HJ>
2021-10-24 21:51:39 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Task2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Task2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int s, v;
+            SqVolQa(a, out s, out v);
+            Console.WriteLine("Площадь {0}, объем {1}", s, v);
+            Console.ReadKey();
+        }
+        static void SqVolQa(int a, out int s, out int v)
+        {
+            s = a * a * 6;
+            v = a * a * a;
+        }
+
+    }
+}
---
+++ Lab7/Task1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину a первого треугольника");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите длину b первого треугольника");
+            int b1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите длину c первого треугольника");
+            int c1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите длину a второго треугольника");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите длину b второго треугольника");
+            int b2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите длину c второго треугольника");
+            int c2 = Convert.ToInt32(Console.ReadLine());
+            if (SqTr(a1, b1, c1) > SqTr(a2, b2, c2))
+            {
+                Console.WriteLine("Площадь первого треугольника больше");
+            }
+            else
+            {
+                if (SqTr(a1, b1, c1) < SqTr(a2, b2, c2))
+                {
+                    Console.WriteLine("Площадь второго треугольника больше");
+                }
+                else
+                {
+                    Console.WriteLine("Площади треугольников равны");
+                }
+            }
+            Console.ReadKey();
+        }
+        static double SqTr(int i1, int i2, int i3)
+        {
+            int p = i1 + i2 + i3;
+            double S = Math.Sqrt(p * (p - i1) * (p - i2) * (p - i3));
+            return S;
+        }
+
+    }
+}
